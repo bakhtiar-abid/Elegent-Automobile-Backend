ca00d15de1ec91d4e3d3a0675b1bfdb0b960797f
bakhtiarabid <abid.bu16@gmail.com>
2021-11-10 19:53:56 +0600

    GET Order API

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
+
+const port = process.env.PORT || 5000;
+
+// middleware
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xztta.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+   useNewUrlParser: true,
+   useUnifiedTopology: true,
+});
+
+console.log(uri);
+
+async function run() {
+   try {
+      await client.connect();
+      console.log("Connected to MongoDB");
+      const database = client.db("elegent_automobile");
+      const ordersCollection = database.collection("orders");
+      const usersCollection = database.collection("users");
+
+      app.get("/orders", async (req, res) => {
+         //  const email = req.query.email;
+         //  const date = req.query.date;
+
+         //  const query = { email: email, date: date };
+
+         const cursor = ordersCollection.find({});
+         const result = await cursor.toArray();
+         res.json(result);
+      });
+   } finally {
+      // await client.close();
+   }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+   res.send("Elegent Automobile Server is running!");
+});
+
+app.listen(port, () => {
+   console.log(`listening at ${port}`);
+});
