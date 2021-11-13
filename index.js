fc39699996791f4f37295880025613c4b882f9c4
bakhtiarabid <abid.bu16@gmail.com>
2021-11-13 17:25:45 +0600

    Make Admin API

--- index.js
+++ index.js
@@ -96,6 +96,30 @@ async function run() {
          res.json(result);
       });
 
+      //ADMIN API
+      app.put("/users/admin", verifyToken, async (req, res) => {
+         const user = req.body;
+         const requester = req.decodedEmail;
+         if (requester) {
+            const requesterAccount = await usersCollection.findOne({
+               email: requester,
+            });
+            if (requesterAccount.role === "admin") {
+               const filter = { email: user.email };
+               const updateDoc = { $set: { role: "admin" } };
+               const result = await usersCollection.updateOne(
+                  filter,
+                  updateDoc
+               );
+               res.json(result);
+            }
+         } else {
+            res.status(403).json({
+               message: "you do not have access to make admin",
+            });
+         }
+      });
+
       //review api
       app.post("/review", async (req, res) => {
          const user = req.body;
